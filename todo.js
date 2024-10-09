ae1b7375a0039855dfd83a330a34e252b5b8d0f3
ShubhamKumar6299 <satyaanandsharma9534@gmail.com>
2024-10-10 02:33:26 +0530

    updated grandparents with grandChild

--- todo.js
+++ todo.js
@@ -6,16 +6,16 @@ function markAsDone(todoId){
 
 function createChild(title, description, id){
     const child = document.createElement("div");
-    const firstGrandParent = document.createElement("div");
-    firstGrandParent.innerHTML = title;
-    const secondGrandParent = document.createElement("div");
-    secondGrandParent.innerHTML = description;
-    const thirdGrandParent = document.createElement("button");
-    thirdGrandParent.innerHTML = "MarkAsDone";
-    thirdGrandParent.setAttribute("onclick",`markAsDone(${id})`);
-    child.appendChild(firstGrandParent);
-    child.appendChild(secondGrandParent);
-    child.appendChild(thirdGrandParent);
+    const firstGrandChild = document.createElement("div");
+    firstGrandChild.innerHTML = title;
+    const secondGrandChild = document.createElement("div");
+    secondGrandChild.innerHTML = description;
+    const thirdGrandChild = document.createElement("button");
+    thirdGrandChild.innerHTML = "MarkAsDone";
+    thirdGrandChild.setAttribute("onclick",`markAsDone(${id})`);
+    child.appendChild(firstGrandChild);
+    child.appendChild(secondGrandChild);
+    child.appendChild(thirdGrandChild);
     child.setAttribute("id",id);
     return child;
 }
